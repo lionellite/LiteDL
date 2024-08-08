c77754e218c7997472742fe7e13530dc9ef72c04
Lionel Lite <liolisena@gmail.com>
2024-08-08 10:41:05 +0100

    18.1 commit replacement

--- LiteDL/LiteDL/settings.py
+++ LiteDL/LiteDL/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'LiteDL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3
+        'ENGINE': 'django.db.backends.sqlite3'
     }
 }
 
