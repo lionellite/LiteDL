bea2274b77175423ac4963b100f64cd939861b4f
Lionel Lite <liolisena@gmail.com>
2024-09-06 19:52:45 +0100

    new

---
+++ LiteDL/views.py
@@ -0,0 +1,90 @@
+from django.views.generic import View
+from pytube import YouTube
+from django.shortcuts import render,redirect
+
+from pytube.innertube import _default_clients
+from pytube import cipher
+import re
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
+
+
+
+def get_throttling_function_name(js: str) -> str:
+    """Extract the name of the function that computes the throttling parameter.
+
+    :param str js:
+        The contents of the base.js asset file.
+    :rtype: str
+    :returns:
+        The name of the function used to compute the throttling parameter.
+    """
+    function_patterns = [
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+    ]
+    #logger.debug('Finding throttling function name')
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            #logger.debug("finished regex search, matched: %s", pattern)
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+                        nfunc=re.escape(function_match.group(1))),
+                    js
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(
+        caller="get_throttling_function_name", pattern="multiple"
+    )
+
+cipher.get_throttling_function_name = get_throttling_function_name
+
+class home(View):
+    def __init__(self,url=None):
+        self.url = url
+    
+    def get(self,request):
+        return render(request,'downloader/home.html') 
+    
+    def post(self,request):
+        # for fetching the video
+        if request.POST.get('fetch-vid'):
+            self.url = request.POST.get('given_url')
+            video = YouTube(self.url)
+            vidTitle,vidThumbnail = video.title,video.thumbnail_url
+            qual,stream = [],[]
+            for vid in video.streams.filter(progressive=True, file_extension='mp4'):
+                qual.append(vid.resolution)
+                stream.append(vid)
+            context = {'vidTitle':vidTitle,'vidThumbnail':vidThumbnail,
+                        'qual':qual,'stream':stream,
+                        'url':self.url}
+            return render(request,'downloader/home.html',context)
+
+        # for downloading the video
+        elif request.POST.get('download-vid'):
+            self.url = request.POST.get('given_url')
+            video = YouTube(self.url)
+            stream = [x for x in video.streams.filter(progressive=True)]
+            video_qual = video.streams[int(request.POST.get('download-vid')) - 1]
+            video_qual.download(output_path='../../Downloads')
+            return redirect('home')
+
+        return render(request,'downloader/home.html')
